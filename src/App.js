c8cf296eb72cfa6db6881cdc0ef012ebb5a15dc1
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-20 08:16:02 +0600

    point add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 function App() {
   return (
@@ -16,11 +16,17 @@ function App() {
 }
 
 function MyComponent(props) {
-  console.log(props)
+  const [points, setPoints] = useState(1);
+
+
+  const hadleAddPoints = () => {
+    setPoints(points * 2)
+  }
   return (
     <div className="component">
       <h1>My component</h1>
-      <h3>Name: {props.name}, Price: {props.price}</h3>
+      <h3>Name: {props.name}, Price: {props.price} and points {points}</h3>
+      <button onClick={hadleAddPoints}>Add Points</button>
       <p className="paragraph">React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.
         Released by Facebook in 2013, React is a JavaScript library for building modern applications. React is used for handling the view layer and can be used for development of both web and mobile applications.</p>
     </div>
