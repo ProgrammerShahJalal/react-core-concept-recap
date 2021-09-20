aa9a70fd2f7fea907f61b541092b896911e14682
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-20 10:17:50 +0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
       <MyComponent name="laptop" price="354664"></MyComponent>
       <MyComponent name="mac pro" price="999999"></MyComponent>
       <MyComponent name="keybord" price="760"></MyComponent>
+      <MyComponent name="book" price="130"></MyComponent>
+      <MyComponent name="mouse" price="460"></MyComponent>
     </div>
   );
 }
@@ -59,8 +61,7 @@ function MyComponent(props) {
   return (
     <div className="component">
       <h1>My component</h1>
-      <h3>Name: {props.name}, Price: {props.price} and
-        <span><br></br></span>  points {points}</h3>
+      <h3>Name: {props.name}, Price: {props.price} and points: {points}</h3>
       <button onClick={hadleAddPoints}>Add Points</button>
       <p className="paragraph">React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.
         Released by Facebook in 2013, React is a JavaScript library for building modern applications. React is used for handling the view layer and can be used for development of both web and mobile applications.</p>
